414ac31a4d97180833690318bad4f6398e7e51ec
kemunga <kemunga@DESKTOP-JUTNGLH>
2022-03-04 10:19:01 +0300

    Add project files.

---
+++ AspNetCoreODataSpatial/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using GeometryWebAPI.Models;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometryWebAPI.Controllers
+{
+    public class CustomersController : ODataController
+    {
+        [EnableQuery]
+        public IQueryable<Customer> Get()
+        {
+            return DataSource.Instance.Customers.AsQueryable<Customer>();
+        }
+
+        [EnableQuery]
+        public Customer Get(int key)
+        {
+            Point redmond = new Point(-122.123889, 47.669444) { SRID = 4326 };
+            return DataSource.Instance.Customers.Where(b => b.Id == key).Single();
+        }
+    }
+}
---
+++ AspNetCoreODataSpatial/Models/Customer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Spatial;
+using NetTopologySuite.Geometries;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeometryWebAPI.Models
+{
+    public class Customer
+    {
+        private GeographyPointWrapper _ptWrapper;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Point Loc
+        {
+            get { return _ptWrapper; }
+            set { _ptWrapper = value; }
+        }
+
+        [NotMapped]
+        public GeographyPoint EdmLoc
+        {
+            get { return _ptWrapper; }
+            set { _ptWrapper = value; }
+        }
+    }
+
+    /*public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Point Loc { get; set; }
+    }*/
+}
